9f531254a650adde2a4e1da315baeaa2e83a30af
Cyrille TOUMI <cyrilletoumi8@gmail.com>
2024-11-26 09:44:48 +0100

    Add Spring doc openAPI

--- src/main/java/com/saga/auth/configuration/SecurityConfig.java
+++ src/main/java/com/saga/auth/configuration/SecurityConfig.java
@@ -40,6 +40,8 @@ public class SecurityConfig {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/swagger-ui/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**").permitAll()
                         .requestMatchers("/api/**").hasAuthority("ADMIN")
                         .anyRequest().authenticated()
                 )
